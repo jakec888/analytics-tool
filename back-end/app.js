43186004f98b01d8b80459765d4d6d4b1524ddd2
jj888 <jaconjcondes@gmail.com>
2019-08-29 23:30:41 -0700

    404 error and 500 error

--- back-end/app.js
+++ back-end/app.js
@@ -8,4 +8,13 @@ app.use(express.json())
 
 app.use('/api', require('./api/url'))
 
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Sorry can't find that!" })
+})
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send({ error: err.stack })
+})
+
 app.listen(port, () => console.log(`http://localhost:${port}`))

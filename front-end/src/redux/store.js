c313ebcfe95f0006163f1faf5b5a16954bb7eab9
jj888 <jaconjcondes@gmail.com>
2019-09-05 11:35:06 -0700

    redux-saga added as middleware to the redux store

--- front-end/src/redux/store.js
+++ front-end/src/redux/store.js
@@ -1,14 +1,33 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import reducers from "./reducers/reducers";
-
+import ReduxThunk from "redux-thunk";
 import { combineReducers } from "redux";
+import rootReducer from "./reducers/reducers";
+import createSagaMiddleware from "redux-saga";
+
+const saga = createSagaMiddleware();
+
+const middlewares = [ReduxThunk, saga];
 
 const composeEnhancers = compose(
-  applyMiddleware(thunk),
+  applyMiddleware(...middlewares),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-const store = createStore(combineReducers(reducers), composeEnhancers);
+const store = createStore(combineReducers(rootReducer), composeEnhancers);
 
 export default store;
+
+// import { createStore, applyMiddleware, compose } from "redux";
+// import thunk from "redux-thunk";
+// import reducers from "./reducers/reducers";
+
+// import { combineReducers } from "redux";
+
+// const composeEnhancers = compose(
+//   applyMiddleware(thunk),
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+
+// const store = createStore(combineReducers(reducers), composeEnhancers);
+
+// export default store;

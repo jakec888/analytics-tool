2fc347d13bde20bf8a8835ee5a795e74ba88d26a
jj888 <jaconjcondes@gmail.com>
2019-09-08 15:30:03 -0700

    switching to create store in the Root.js file to see if integration testing works

--- front-end/src/Root.js
+++ front-end/src/Root.js
@@ -1,7 +1,30 @@
 import React from "react";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+
+import { createStore, applyMiddleware, compose } from "redux";
+import { combineReducers } from "redux";
+import createSagaMiddleware from "redux-saga";
+import ReduxThunk from "redux-thunk";
+import rootReducer from "./redux/rootReducers";
+import rootSagas from "./redux/rootSaga";
 
 export default ({ children, initialState = {} }) => {
+  const ReduxSaga = createSagaMiddleware();
+
+  const middlewares = [ReduxThunk, ReduxSaga];
+
+  const composeEnhancers = compose(
+    applyMiddleware(...middlewares),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+
+  const store = createStore(
+    combineReducers(rootReducer),
+    initialState,
+    composeEnhancers
+  );
+
+  ReduxSaga.run(rootSagas);
+
   return <Provider store={store}>{children}</Provider>;
 };
--- front-end/src/redux/store.js
+++ front-end/src/redux/store.js
@@ -1,36 +1,21 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import ReduxThunk from "redux-thunk";
-import { combineReducers } from "redux";
-import rootReducer from "./reducers";
-import createSagaMiddleware from "redux-saga";
-import rootSagas from "./saga";
-
-const ReduxSaga = createSagaMiddleware();
-
-const middlewares = [ReduxThunk, ReduxSaga];
-
-const composeEnhancers = compose(
-  applyMiddleware(...middlewares),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-
-const store = createStore(combineReducers(rootReducer), composeEnhancers);
-
-ReduxSaga.run(rootSagas);
-
-export default store;
-
 // import { createStore, applyMiddleware, compose } from "redux";
-// import thunk from "redux-thunk";
-// import reducers from "./reducers/reducers";
-
+// import ReduxThunk from "redux-thunk";
 // import { combineReducers } from "redux";
+// import rootReducer from "./reducers";
+// import createSagaMiddleware from "redux-saga";
+// import rootSagas from "./saga";
+
+// const ReduxSaga = createSagaMiddleware();
+
+// const middlewares = [ReduxThunk, ReduxSaga];
 
 // const composeEnhancers = compose(
-//   applyMiddleware(thunk),
+//   applyMiddleware(...middlewares),
 //   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 // );
 
-// const store = createStore(combineReducers(reducers), composeEnhancers);
+// const store = createStore(combineReducers(rootReducer), {}, composeEnhancers);
+
+// ReduxSaga.run(rootSagas);
 
 // export default store;

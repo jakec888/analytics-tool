682f5706609f20b0f76f5c9fc692dfc00c550244
jj888 <jaconjcondes@gmail.com>
2019-09-09 18:43:44 -0700

    removed 'window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()' and switched with composeWithDevTools as it works without breaking my tests

--- front-end/src/Root.js
+++ front-end/src/Root.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Provider } from "react-redux";
-
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { combineReducers } from "redux";
 import createSagaMiddleware from "redux-saga";
 import ReduxThunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
 import rootReducer from "./redux/rootReducers";
 import rootSagas from "./redux/rootSaga";
 
@@ -13,11 +14,7 @@ export default ({ children, initialState = {} }) => {
 
   const middlewares = [ReduxThunk, ReduxSaga];
 
-  const composeEnhancers = compose(
-    applyMiddleware(...middlewares)
-    // comment this when testing
-    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  );
+  const composeEnhancers = composeWithDevTools(applyMiddleware(...middlewares));
 
   const store = createStore(
     combineReducers(rootReducer),

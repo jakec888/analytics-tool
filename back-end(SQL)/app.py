4ecf87fc1e74f127561592633c1b329170354da7
jj888 <jaconjcondes@gmail.com>
2019-09-10 22:55:28 -0700

    choose orm and created models

--- back-end(SQL)/app.py
+++ back-end(SQL)/app.py
@@ -1,7 +1,14 @@
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+
+db = SQLAlchemy(app)
 
 
 @app.route('/')
---
+++ back-end(SQL)/models.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from app import db
+
+
+class Link(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.String(80), nullable=False)
+    redirectId = db.Column(db.String(80), nullable=False)
+    redirectURL = db.Column(
+        db.String(80), nullable=False, default=datetime.utcnow)
+    link = db.Column(db.String(80), nullable=False)
+    title = db.Column(db.String(80), nullable=False)
+    date = db.Column(db.String(80), nullable=False)
+
+    data_id = db.Column(db.Integer, db.ForeignKey(
+        'data.id'), nullable=False)
+    data = db.relationship(
+        'Data', backref=db.backref('link', lazy=True))
+
+
+class Data(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(80), nullable=False)
+    clicks = db.Column(db.Integer, nullable=False)

31cba3337c0e7244ed8c054d635ff6c7c708aeb0
jj888 <jaconjcondes@gmail.com>
2019-09-25 12:56:11 -0700

    created migration tables and successfully linked sqlite3

---
+++ SQL_version/php_version/back-end/database/migrations/2019_09_25_195106_create_links_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $userId->string('userId');
+            $userId->string('redirectId');
+            $userId->string('redirectURL');
+            $userId->string('link');
+            $userId->string('title');
+            $userId->string('date');
+            $userId->string('RELATIONSHIP');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('links');
+    }
+}

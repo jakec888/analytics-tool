48963a835106786836c95d746a4299cf5635d56c
jj888 <jaconjcondes@gmail.com>
2019-12-13 17:33:55 -0800

    added redux, redux-saga, redux-thunk, redux-devtools-extension

--- RoR/app/javascript/bundles/HelloWorld/store/helloWorldStore.js
+++ RoR/app/javascript/bundles/HelloWorld/store/helloWorldStore.js
@@ -1,8 +1,20 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import helloWorldReducer from '../reducers/helloWorldReducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
+import thunk from 'redux-thunk';
+
+const ReduxSaga = createSagaMiddleware();
+
+const middlewares = [
+  thunk,
+  ReduxSaga,
+];
+
+const composeEnhancers = composeWithDevTools(applyMiddleware(...middlewares));
 
 const configureStore = (railsProps) => (
-  createStore(helloWorldReducer, railsProps)
+  createStore(helloWorldReducer, railsProps, composeEnhancers)
 );
 
 export default configureStore;
